8e4152036a4d167e71ba0f80455533f9bec48ad3
junyeong <whwnsdud4592@naver.com>
2025-03-13 15:21:27 +0900

    feat(InputView) InputView메서드 구현

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -1,5 +1,6 @@
 package View;
 
+import Controller.TryNumber;
 import Io.Reader;
 import Cars.Cars;
 
@@ -8,5 +9,10 @@ public class InputView {
   public Cars readCarNames(){
     String input = Reader.readLine();
     return new Cars(input);
+  }
+
+  public TryNumber readTryNumbers(){
+    String input = Reader.readLine();
+    return new TryNumber(input);
   }
 }
