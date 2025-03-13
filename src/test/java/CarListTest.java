a01d23462efd5694fd2e48aa03b0e0aab3127632
junyeong <whwnsdud4592@naver.com>
2025-03-13 09:15:21 +0900

    feat(CarListTest) Test Case추가

--- src/test/java/CarListTest.java
+++ src/test/java/CarListTest.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import Car.Car;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class CarListTest {
 
   @Test
@@ -15,7 +17,24 @@ public class CarListTest {
     String input = "jun,han";
     CarList carList = new CarList(input);
     List<Car> expected = Arrays.asList(new Car("jun"), new Car("han"));
-    carList.printAllelement();
     assertThat(carList.getCarList()).containsExactlyInAnyOrderElementsOf(expected);
+  }
+
+  @Test
+  @DisplayName("자동차의 이름이 5자리 보다 클 때 객체 생성")
+  void 객체생성2(){
+    String input = "jun,hanjunju";
+    assertThrows(IllegalArgumentException.class, () -> {
+      new CarList(input);
+    });
+  }
+
+  @Test
+  @DisplayName("자동차의 이름이 빈 값으로 들어갈 때 객체 생성")
+  void 객체생성3(){
+    String input ="     ";
+    assertThrows(IllegalArgumentException.class, () -> {
+      new CarList(input);
+    });
   }
 }
